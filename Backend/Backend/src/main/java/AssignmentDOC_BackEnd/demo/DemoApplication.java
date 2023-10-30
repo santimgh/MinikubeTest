d6d133b624531044f79f63477e7238da2a01e63b
Santiago Gomez <santimiguelgomez@gmail.com>
2023-10-30 09:41:06 +0100

    For minikube using githubactions

---
+++ Backend/Backend/src/main/java/AssignmentDOC_BackEnd/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package AssignmentDOC_BackEnd.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ Backend/Backend/src/test/java/AssignmentDOC_BackEnd/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package AssignmentDOC_BackEnd.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
